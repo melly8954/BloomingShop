3d240a68e774a3d2792baef280b265c97388da9b
haily <49448330+melly8954@users.noreply.github.com>
2025-01-12 18:42:23 +0900

    250112 프로젝트 초안

---
+++ src/main/java/com/melly/bloomingshop/BloomingShopApplication.java
@@ -0,0 +1,13 @@
+package com.melly.bloomingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomingShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomingShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melly/bloomingshop/BloomingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.bloomingshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomingShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
