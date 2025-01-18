baf2e887040c96e02e1cef3219be59c525121b5b
haily <49448330+melly8954@users.noreply.github.com>
2025-01-18 23:38:53 +0900

    250118 request  dto 추가

---
+++ src/main/java/com/melly/bloomingshop/dto/LoginIdCheckDto.java
@@ -0,0 +1,10 @@
+package com.melly.bloomingshop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginIdCheckDto {
+    private String loginId;
+}
---
+++ src/main/java/com/melly/bloomingshop/dto/PhoneNumberCheckDto.java
@@ -0,0 +1,10 @@
+package com.melly.bloomingshop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhoneNumberCheckDto {
+    private String phoneNumber;
+}
