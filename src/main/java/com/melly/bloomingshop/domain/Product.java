b3c273d494309ac2de1abbede7fc293186151aae
haily <49448330+melly8954@users.noreply.github.com>
2025-01-20 06:38:07 +0900

    250120 필드명 수정

--- src/main/java/com/melly/bloomingshop/domain/Product.java
+++ src/main/java/com/melly/bloomingshop/domain/Product.java
@@ -14,7 +14,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
-    private Long productId;
+    private Long id;
 
     @Column(name = "name", nullable = false, length = 100)
     private String name;
