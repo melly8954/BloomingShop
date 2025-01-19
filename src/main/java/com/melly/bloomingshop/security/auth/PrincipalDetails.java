67dd34e932e0e792370703808d265f3705080e09
haily <49448330+melly8954@users.noreply.github.com>
2025-01-20 00:14:25 +0900

    250120 구글 OAuth2.0 로그인을 위한 클래스 추가

--- src/main/java/com/melly/bloomingshop/security/auth/PrincipalDetails.java
+++ src/main/java/com/melly/bloomingshop/security/auth/PrincipalDetails.java
@@ -5,6 +5,7 @@ import com.melly.bloomingshop.domain.User;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,7 +16,7 @@ import java.util.Map;
 // UserDetails 는 실제로 사용자 정보를 담고 있는 인터페이스
 // UserDetails 인터페이스를 구현한 커스텀 클래스, Spring Security 에서 사용자 인증 및 권한 관리에 사용되는 클래스
 @Getter
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
     private Map<String, Object> attributes;
@@ -48,6 +49,15 @@ public class PrincipalDetails implements UserDetails {
         return user.getEmail();  // 이메일을 가져오는 메서드
     }
 
+    // 사용자의 성별 반환
+    public String getGender(){
+        return user.getGender();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     // 사용자의 권한을 반환
     @Override
@@ -85,5 +95,10 @@ public class PrincipalDetails implements UserDetails {
         // 현재 시간 - 계정 로그인 시간 >> 1년을 초과 시  return false;
 
         return true;    // 계정 활성화 여부, true는 활성화된 계정
+    }
+
+    @Override
+    public String getName() {
+        return "";
     }
 }
