3d6dc4c8becbac3f05f8b3b3c0f2eaa9e9090b6d
haily <49448330+melly8954@users.noreply.github.com>
2025-01-20 00:32:32 +0900

    250120 OAuth2 로그인 실패 시 redirect 되는 view 추가

--- src/main/java/com/melly/bloomingshop/controller/UserController.java
+++ src/main/java/com/melly/bloomingshop/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.melly.bloomingshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -13,5 +15,12 @@ public class UserController {
     @GetMapping("/user/login")
     public String loginPage() {
         return "user/login";
+    }
+
+    // OAuth2 로그인 실패 시 redirect 되는 view , CustomAuthenticationFailureHandler 가 관리
+    @GetMapping("/login-fail")
+    public String loginFail(@RequestParam("error") String errorMessage, Model model) {
+        model.addAttribute("error", errorMessage);
+        return "user/login_fail";  // Mustache template file
     }
 }
