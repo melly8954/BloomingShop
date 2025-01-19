c6002d76a567b367be82ee200e828f771a02ab14
haily <49448330+melly8954@users.noreply.github.com>
2025-01-20 06:46:16 +0900

    250120 @Repository 어노테이션 추가

--- src/main/java/com/melly/bloomingshop/repository/AddressRepository.java
+++ src/main/java/com/melly/bloomingshop/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.melly.bloomingshop.repository;
 
 import com.melly.bloomingshop.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
