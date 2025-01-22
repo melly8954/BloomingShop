89b169a5037f5f5fdaecff0e3008a8b72858e826
haily <49448330+melly8954@users.noreply.github.com>
2025-01-23 03:00:39 +0900

    250123 관리자 페이지 상품 수정 url

--- src/main/java/com/melly/bloomingshop/controller/AdminController.java
+++ src/main/java/com/melly/bloomingshop/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.melly.bloomingshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,9 +24,10 @@ public class AdminController {
         return "admin/product/product_add_admin";
     }
 
-    @GetMapping("/product/modify")
-    public String productModifyPage() {
+    @GetMapping("/product/modify/{productId}")
+    public String productModifyPage(@PathVariable Long productId, Model model) {
+        // 필요하면 productId를 기반으로 데이터 로드 후 Model에 추가
+        model.addAttribute("productId", productId);
         return "admin/product/product_modify_admin";
     }
-
 }
