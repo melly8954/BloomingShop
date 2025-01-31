628e6130b08c68c46448b71d1f4041e212577022
haily <49448330+melly8954@users.noreply.github.com>
2025-01-31 18:35:43 +0900

    250131 주문내역 로그인,비로그인 id 와 deleted_flag 로 주문id 찾기 Repository 수정

--- src/main/java/com/melly/bloomingshop/repository/OrderRepository.java
+++ src/main/java/com/melly/bloomingshop/repository/OrderRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(User userId);   // 로그인한 유저의 주문 내역 조회
-    List<Order> findByGuestId(String guestId); // 비로그인 유저의 주문 내역 조회
+    List<Order> findByUserIdAndDeletedFlagFalse(User userId);   // 로그인한 유저의 삭제되지 않은 주문 내역 조회
+    List<Order> findByGuestIdAndDeletedFlagFalse(String guestId); // 비로그인 유저의 삭제되지 않은 주문 내역 조회
 }
