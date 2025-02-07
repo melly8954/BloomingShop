955b7d6c4b4866305bc50192bd979e49e04c8c38
haily <49448330+melly8954@users.noreply.github.com>
2025-02-07 21:26:53 +0900

    250207 index 화면 업데이트
    관리자 계정의 index 화면 업데이트

--- src/main/java/com/melly/bloomingshop/controller/IndexController.java
+++ src/main/java/com/melly/bloomingshop/controller/IndexController.java
@@ -16,30 +16,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
-        // SecurityContext에서 인증 객체를 가져옴
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.isAuthenticated()) {
             Object principal = authentication.getPrincipal();
 
             if (principal instanceof PrincipalDetails principalDetails) {
-                // PrincipalDetails에서 UserEntity를 가져옴
                 User loginUser = principalDetails.getUser();
                 model.addAttribute(SecurityConfig.LOGINUSER, loginUser);
                 model.addAttribute("isAuthenticated", true);
+
+                if ("ADMIN".equals(loginUser.getRoleId().getRoleName())) {
+                    model.addAttribute("isAdmin", true);  // Mustache에서 사용할 값 추가
+                } else {
+                    model.addAttribute("isAdmin", false);
+                }
+
             } else if (principal instanceof DefaultOAuth2User oauthUser) {
-                // OAuth2 사용자의 경우 (OAuth2UserService로 설정한 사용자 정보에서 추출)
                 String name = oauthUser.getAttribute("name");
                 model.addAttribute(SecurityConfig.LOGINUSER, name);
                 model.addAttribute("isAuthenticated", true);
+                model.addAttribute("isAdmin", false);
             } else {
-                // 인증되지 않은 사용자
                 model.addAttribute("isAuthenticated", false);
+                model.addAttribute("isAdmin", false);
             }
         } else {
-            // 인증 객체가 없거나 인증되지 않은 경우
             model.addAttribute("isAuthenticated", false);
+            model.addAttribute("isAdmin", false);
         }
         return "index";
     }
+
 }
